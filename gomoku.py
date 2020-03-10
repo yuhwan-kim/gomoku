7c01cad7089f8867f18db5cb0ca979dde8e1c5c6
yuhwan-kim <45034615+yuhwan-kim@users.noreply.github.com>
2020-03-10 21:56:37 +0900

    Start and Template of Gomoku
    Start and Template of Gomoku: Gomoku project will start from here

---
+++ gomoku.py
@@ -0,0 +1,17 @@
+#initialize
+blank = 0
+white = 1
+black = 2
+board_size = 15
+board = [[blank for _ in range(board_size)] for _ in range(board_size)]
+
+
+while(True):
+    pass
+    #get coordinates
+    #print board
+    #change turn
+
+
+
+#print winner and board
